d077838938cd12d4519181705f2d251a9ac94bfe
Daniel <danielbenicio123@gmail.com>
2022-01-04 13:37:50 -0300

    feat: add function to implement toUpperCase

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//DOM Objects
 const mainScreen = document.querySelector('.main-screen')
 const pokeName = document.querySelector('.poke-name');
 const pokeId = document.querySelector('.poke-id');
@@ -8,33 +9,62 @@ const pokeTypeTwo = document.querySelector('.poke-type-two')
 const pokeWeight = document.querySelector('.poke-weight')
 const pokeHeight = document.querySelector('.poke-height')
 
+//Constants e Variables
 const apiURL = 'https://pokeapi.co/api/v2/pokemon/1'
+const TYPES = [
+  'normal', 'fighting', 'flying',
+  'poison', 'ground', 'rock',
+  'bug', 'ghost', 'steel',
+  'fire', 'water', 'grass',
+  'electric', 'psychic', 'ice',
+  'dragon', 'dark', 'fairy'
+];
 
+//Functions
+const capitalize = (str) => str[0].toUpperCase() + str.substr(1);
+
+const resetScreen = () => {
+  mainScreen.classList.remove('hide')
+  for (const type of TYPES) {
+    mainScreen.classList.remove(type)
+  }
+}
+
+//Fetch
 const fetchData = async url  => {
   const res = await fetch(`${apiURL}`)
   const data = await res.json()
-  console.log(data)
+  resetScreen()
 
-  mainScreen.classList.remove('hide')
-  pokeName.textContent = data['name']
-  pokeId.textContent = data['id']
-  pokeWeight.textContent = data['weight']
-  pokeHeight.textContent = data['height']
-
-  const dataTypes = data['types']
-  const dataFirstType = dataTypes[0]
-  const dataSecondType = dataTypes[1]
-  pokeTypeOne.textContent = dataFirstType['type']['name']
-
+  const dataTypes = data['types'];
+  const dataFirstType = dataTypes[0];
+  const dataSecondType = dataTypes[1];
+  pokeTypeOne.textContent = capitalize(dataFirstType['type']['name']);
   if(dataSecondType) {
-    pokeTypeTwo.classList.remove('hide')
-    pokeTypeTwo.textContent = dataSecondType['type']['name']
+    pokeTypeTwo.classList.remove('hide');
+    pokeTypeTwo.textContent = capitalize(dataSecondType['type']['name']);
   } else {
-    pokeTypeTwo.classList.add('hide')
+    pokeTypeTwo.classList.add('hide');
     pokeTypeTwo.textContent = '';
   }
+  mainScreen.classList.add(dataFirstType['type']['name']);
+  pokeName.textContent = capitalize(data['name']);
+  pokeId.textContent = '#' + data['id'].toString().padStart(3, '0');
+  pokeWeight.textContent = data['weight'];
+  pokeHeight.textContent = data['height'];
+  pokeFrontImage.src = data['sprites']['front_default'];
+  pokeBackImage.src = data['sprites']['back_default'];
 }
 fetchData()
+
+
+
+
+
+
+
+
+
 
 // fetch('https://pokeapi.co/api/v2/pokemon/1') //res => response
 //   .then(res => res.json())
